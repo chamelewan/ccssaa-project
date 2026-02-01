bb7f5a49f468ad295f3e93b5745082b412bf93d7
김승완 <wani001110@naver.com>
2026-02-01 19:37:07 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/ccssaa/project/CcssaaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ccssaa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcssaaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcssaaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccssaa/project/CcssaaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccssaa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcssaaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
